de6f4dfe08a6d35942ada4996ca929ea61ee41b6
NourTebourski <79874265+NourTebourski@users.noreply.github.com>
2021-06-02 21:58:50 +0100

    adding reaccompcheckpoint

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+      <img
+        src="https://scontent.ftun3-1.fna.fbcdn.net/v/t1.6435-9/193188950_4124723687635293_6998001574250933288_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=gnZk-lhhC0EAX9ZHY43&_nc_ht=scontent.ftun3-1.fna&oh=f604af2e7f961033d0f735b8fac5b838&oe=60DDA34B"
+        width="300"
+        height="300"
+      />
+    </div>
+  );
+};
+
+export default ProfilePhoto;
